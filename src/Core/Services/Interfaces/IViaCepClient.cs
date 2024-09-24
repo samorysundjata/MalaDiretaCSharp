c3f7cca18e5d01caa111ba45ff23b2509584a4d6
Samory Sundjata <samory.sundjata@gmail.com>
2024-09-24 17:35:40 -0300

    Interfaces

---
+++ src/Core/Services/Interfaces/IViaCepClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Interfaces
+{
+    public interface IViaCepClient
+    {
+        ViaCepResult Search(string zipCode);
+
+        IEnumerable<ViaCepResult> Search(string stateInitials, string city, string address);
+
+        Task<ViaCepResult> SearchAsync(string zipCode, CancellationToken cancellationToken);
+
+        Task<IEnumerable<ViaCepResult>> SearchAsync(
+            string stateInitials,
+            string city,
+            string address,
+            CancellationToken cancellationToken
+        );
+    }
+}
---
+++ src/Infra/Repositories/Interfaces/IDestinatarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+namespace Infra.Repositories.Interfaces
+{
+    public interface IDestinatarioRepository : IRepository<Destinatario>
+    {
+        Endereco Get(int id);
+
+        Task<IEnumerable<Destinatario>> GetDestinatarios();
+    }
+}
